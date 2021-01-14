3c8d6cd9b3e12f865b51e7ec0aa658aab6f3bc5a
Tongyao <58587822+super-tongyao@users.noreply.github.com>
2021-01-14 13:16:49 +0800

    Update tongyao_player-1.1.js

--- js/tongyao_player-1.1.js
+++ js/tongyao_player-1.1.js
@@ -14,11 +14,6 @@ $(function (){
 	})
 	setTimeout(function (){
 		$("#tongyao-music").css("left","10px")
-		setTimeout(function (){
-			$("#tongyao-music").css("left","-325px")
-			$("#tongyao-left").html("&lt;")
-			i = 1;
-		},5000)
 	},1000)
 	console.log('%cTongYao_Player','font-size:2em');
 	console.log(decodeURI("%E8%AF%B7%E5%8A%A1%E5%BF%85%E4%BF%9D%E7%95%99Tongyao_Player%E4%BD%9C%E8%80%85%E7%89%88%E6%9D%83%E4%BF%A1%E6%81%AF%E3%80%82%E5%8D%9A%E5%AE%A2%E5%9C%B0%E5%9D%80%EF%BC%9A")+"http://blog.yaoyaoman.cn \n\nGithub:https://github.com/super-tongyao/Tongyao_Player.git ")
